b1dff907db1faa57445f58e6a300fb8c1460eace
Priyobrato Das <priyodas12@gmail.com>
2022-08-03 18:48:07 +0530

    Initial commit

---
+++ src/main/java/com/example/springboottotoisegit/SpringbootTotoiseGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottotoisegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTotoiseGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTotoiseGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboottotoisegit/SpringbootTotoiseGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottotoisegit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTotoiseGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
